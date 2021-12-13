1133a920dddc7e51d340a463979dd21fb215ffff
carlgronvald <carl@gronvald.com>
2021-12-13 14:17:38 +0100

    :fire: Remove approximate triangle collision code

--- src/logic/collision/triangle.rs
+++ src/logic/collision/triangle.rs
@@ -187,36 +187,6 @@ impl Triangle {
         false
     }
 
-    pub fn approx_is_colliding(&self, aabb: &Aabb) -> bool {
-        if !self.surrounding_aabb.is_colliding(aabb) {
-            return false;
-        }
-
-        for side in TriangleSide::iter() {
-            let vector_to_side = self.vector_to_side(side);
-            let distance_to_side = self.distance_to_side(side, &vector_to_side);
-
-            for corner in AabbCorner::iter() {
-                let corner = aabb.get_corner(corner);
-                let projected_distance = (corner - self.center_point).dot(&vector_to_side);
-                if projected_distance > 0.0 && projected_distance < distance_to_side {
-                    return true;
-                }
-            }
-        }
-
-        for corner in TriangleCorner::iter() {
-            if aabb.is_inside(self.get_corner(corner)) {
-                return true;
-            }
-        }
-
-        // NB: THIS DOES NOT CATCH ALL INTERSECTIONS! IF THE TRIANGLE AND RECTANGLE ARE OVERLAPPING WITHOUT ANY CORNERS INSIDE EACH OTHER, IT'S MISSED!
-        // BUT THIS REQUIRES SWIFT MOVEMENT FROM EITHER THE TRIANGLE OR THE RECTANGLE; THIS ALGORITHM IS NOT MADE TO CATCH THAT
-
-        false
-    }
-
     pub fn get_corner(&self, corner: TriangleCorner) -> Vec2 {
         match corner {
             TriangleCorner::A => self.a,
